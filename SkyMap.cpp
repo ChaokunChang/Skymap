e99b85c77d41f3db00f0020787b1c14347483a42
fdcck <16307130138@fudan.edu.cn>
2019-04-08 15:37:27 +0800

    delete main.cpp and add skymap.class

---
+++ SkyMap.cpp
@@ -0,0 +1,21 @@
+#include "SkyMap.h"
+
+void SkyMap:: run(){
+	ImageProcessing IP("../Data/source001.png","../Data/source001.csv");
+   	string processed_picture = IP.Process();
+   	if(processed_picture == "") return 0;
+
+    SkyMapMatching SMM;
+    string dataset = "../Data/skymaps.csv";
+    string picture = "../Data/observation_00.csv";
+    SMM.LoadSky(dataset);
+    SMM.LoadImage(picture);
+    StarPoint c(0,140.0,0.0,0.0);
+    float len = 20.0,width = 20.0;
+    SMM.GenerateSimImage(c,len,width);
+    //SMM.GenerateSimImage(c,1,20);
+    SMM.SelectTargetStar();
+    SMM.Match();
+    if( SMM.Check() ) cout<<"Good!"<<endl;
+    else cout<<"Sad!"<<endl;
+}
---
+++ SkyMap.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ImageProcessing.h"
+#include "SkyMapMatching.h"
+#include <malloc.h>
+#include <algorithm>
+using namespace std;
+
+
+class SkyMap{
+public:
+	SkyMap();
+	void Run();
+}
