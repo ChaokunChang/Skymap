b2b79d98cb8498ed5658ed05f4db2697c63f979c
ZhaoWeicheng98 <zhaoweicheng98@gmail.com>
2019-04-08 12:55:46 +0800

    Frontend Framework added.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,11 @@
-#include <iostream>
+#include "mainwindow.h"
+#include <QApplication>
 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
 
-#include "SkyMapMatching.h"
-
-using namespace std;
-
-int main() {
-//    int range1 = 10;
-//    float range2 = 10.0;
-//    float range3 = 3.2;
-//    float x1 = 34.78;
-//    cout<<fmod(x1,range1)<<endl;
-//    cout<<fmod(x1,range2)<<endl;
-//    cout<<fmod(x1,range3)<<endl;
-
-    SkyMapMatching SMM;
-
-    string filename = "D:\\Program Files\\CLion\\SkyMap\\Data\\skymaps.csv";
-    string picture = "D:\\Program Files\\CLion\\SkyMap\\Data\\observation_00.csv";
-    SMM.LoadSky(filename);
-    SMM.LoadImage(picture);
-    StarPoint c(0,10.0,40.0,0.0);
-    float len = 15.0,width = 15.0;
-    SMM.GenerateSimImage(c,len,width);
-    SMM.Match();
-
-    return 0;
+    return a.exec();
 }
