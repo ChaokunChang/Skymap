08c5d952fb6156e92160d1be55499cfedde3e64f
Zhongyu Chen <zhongyuchen@yahoo.com>
2019-04-15 09:29:21 +0800

    set mean max/gradient min preprocess img

--- include/extraction.h
+++ include/extraction.h
@@ -2,7 +2,7 @@
 #define EXTRACTION_H
 
 #include <iostream>
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
 #include <vector>
 
 // macros
@@ -10,23 +10,30 @@
 #define ALPHA 5
 #define STARS_MAX 200
 #define FOOTSTEP 40
+#define LOCAL_BLOCK 3
+#define LOCAL_NUM 30
+#define MEAN_BLOCK 5
+#define MEAN_MAX 100
+#define GRADIENT_MAX 3
 
-using namespace cv;
 using namespace std;
-
+using namespace cv;
 
 // functions
 vector<pair<int, int>> projection_row(Mat &segimg);
 vector<pair<int, int>> projection_col(Mat &segimg);
 Mat read_img(const string &filename);
 double background_threshold(Mat &img);
+double local_background_threshold(Mat &img);
+Mat gradient_filter(Mat img);
+Mat mean_filter(Mat img);
 Mat preprocess_img(Mat &srcimg);
-void print_vector(vector<pair<double, double>> &v);
+void print_vector(vector<pair<pair<double, double>, double>> &v);
 int block_sum_weight(int i, int j, pair<int, int> &px, pair<int, int> &py, const char &multiple);
-double block_sum(Mat &img, pair<int, int> &px, pair<int, int> &py, const char &multiple);
-vector<pair<double, double>> get_centroids(Mat &img);
-Mat plot_centroids(Mat plotimg, vector<pair<double, double>> &centroids, const uchar color[]);
-Mat plot_centroids_white_background(Mat &img, vector<pair<double, double>> &centroids, const uchar color[]);
+double block_sum(Mat &img, pair<int, int> &px, pair<int, int> &py, const char &multiple, const double &T);
+vector<pair<pair<double, double>, double>> get_centroids(Mat &img);
+Mat plot_centroids(Mat plotimg, vector<pair<pair<double, double>, double>> &centroids, const uchar color[]);
+Mat plot_centroids_white_background(Mat &img, vector<pair<pair<double, double>, double>> &centroids, const uchar color[]);
 Mat star_filter(Mat &img, const int &max, const double &footstep);
 
 #endif //EXTRACTION_H
